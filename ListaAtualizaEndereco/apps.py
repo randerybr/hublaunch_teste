8137e2ec1c8ff346124195607f474b3911a4700d
Rander Rodrigues (GSTI) <randerybr@gmail.com>
2021-08-27 22:32:45 -0300

    Commit

---
+++ ListaAtualizaEndereco/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ListaatualizaenderecoConfig(AppConfig):
+    name = 'ListaAtualizaEndereco'
---
+++ ListaAtualizaEndereco/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=200)
+    NomeMae = models.CharField(max_length=200)
+    datanascimento = models.DateField(blank=True, null=True)
+    dataCadastro = models.DateField(default=timezone.now)
+
+    def incluir(self):
+        self.save()
+
+    def __str__(self):
+        return self.nome
